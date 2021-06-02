95266a42250e4971c89669badcf3054546898402
Anton <antonshvetswork@yandex.ru>
2021-06-02 22:16:16 +0300

    First commit

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+
+
+let Rock = document.getElementById('rock');
+let Paper = document.getElementById('paper');
+let Scissors = document.getElementById('scissors');
+
+Rock.addEventListener('click', function(){
+
+    alert("Вы выбрали камень")
+});
+
+Paper.addEventListener('click', function(){
+
+    alert("Вы выбрали бумагу")
+});
+
+Scissors.addEventListener('click', function(){
+
+    alert("Вы выбрали ножницы")
+});
